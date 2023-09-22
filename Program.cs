321a2a3d53e46e9f9c66ff7d96bc564fadf5f689
Yakimov Lev <tiokuhytblu@gmail.com>
2023-09-22 21:17:37 +0500

    123

--- Program.cs
+++ Program.cs
@@ -4,8 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+namespace MySpace
+{
+    
+}
+
+
+
 namespace ConsoleApp4
 {
+    struct Student
+    {
+        public string name;
+        public readonly int age;
+    }
+
+    public class MyClass
+    {
+        public int a;
+        public int b;
+        
+        public MyClass(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void SetA(int a) 
+        { 
+            this.a = a;
+        }
+
+        public int Summ(int a, int b)
+        {
+            return a + b;
+        }
+
+        
+    }
+
+   
+
     internal class Program
     {
         static void Main(string[] args)
@@ -16,9 +55,70 @@ namespace ConsoleApp4
 
             //SetUpFirstLetter();
 
-            HideInvalidWords();
+            //HideInvalidWords();
+
+            //MyClass obj = new MyClass();
+            //Console.WriteLine(obj.name);
+            //Console.WriteLine(obj.age);
+
+
+            //int num = 1;
+            //int a = 3;
+            //int b = 5;
+            //Console.WriteLine(summ(ref a, ref b, out num));
+
+            //int a = 1;
+            //int b = 2;
+            //MyClass myClass = new MyClass(1,2);
+            //Console.WriteLine(myClass.Summ(a, b));
+
+
+            Employee emp1 = new Employee();
+            Employee emp2 = new Employee("Сидоров Иван Васильевич", "15.05.1980", "8-950-540-91-61", "sidor@gmail.com", "стропальщик", "работа по строповке грузов");
+            Employee emp3 = new Employee();
+
+            emp1.SetFIO("Иванов Пётр Петрович");
+            emp1.SetBirthDate("14.05.1979");
+            emp1.SetTelephoneNumber("8-916-122-62-33");
+            emp1.SetEmail("ivanov@mail.ru");
+            emp1.SetJobTitle("крановщик");
+            emp1.SetJobDescription("работа на мостовом кране");
+            emp1.PrintEmployee();
+            Console.WriteLine();
+            emp2.PrintEmployee();
+
+            //emp3.Set.set("Игорь Владимирович Сорокин", "", "", "", "", "");
+            //Console.WriteLine(emp3.GetFIO());
+
+            //Shape shape= new Shape();
+            //double hight = 0;
+            //double width = 0;
+            //double length = 0;
+            //Console.WriteLine("Введите длину стороны куба: ");
+            //hight = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Площадь сторон куба равна - " + shape.SquareOf(hight));
+
+
+            //Console.WriteLine("Введите длину параллелепипеда:");
+            //hight = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите ширину параллелепипеда:");
+            //width = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите высоту параллелепипеда:");
+            //length = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Площадь сторон параллелепипеда равна - " + shape.SquareOf(hight, width, length));
+
+            Distance d = new Distance();
+            d.Print();
+            Console.WriteLine(d.MaxOf());
 
         }
+
+        public static void summ(ref int a, ref int b, out int s)
+        {
+            s = a + b;
+        }
+
+       
 
         static void CesarSifr() // шифр Цезаря
         {
@@ -396,5 +496,134 @@ namespace ConsoleApp4
             Console.WriteLine("Статистика: " + countOfInvalidWord + " замены слова " + invalidWord + ".");
         }
 
+    }
+
+    public class Employee
+    {
+        private string FIO;
+        private string BirthDate;
+        private string TelephoneNumber;
+        private string Email;
+        private string JobTitle;
+        private string JobDescription;
+
+        public Employee() { }
+
+        public Employee(string FIO, string BirthDate, string TelephoneNumber, string Email, string JobTitle, string JobDescription) 
+        {
+            this.FIO = FIO;
+            this.BirthDate = BirthDate;
+            this.TelephoneNumber = TelephoneNumber;
+            this.Email = Email;
+            this.JobTitle = JobTitle;
+            this.JobDescription = JobDescription;
+        }
+
+        public void PrintEmployee()
+        {
+            Console.WriteLine("Фамилия Имя Отчество: " + FIO);
+            Console.WriteLine("Дата рождения: " + BirthDate);
+            Console.WriteLine("Контактный телефон: " + TelephoneNumber);
+            Console.WriteLine("Электронная почта: " + Email);
+            Console.WriteLine("Должность: " + JobTitle);
+            Console.WriteLine("Описание должности: " + JobDescription);
+        }
+
+        public string GetFIO()
+        {
+            return FIO;
+        }
+
+        public string GetBirthDate()
+        {
+            return BirthDate;
+        }
+
+        public string GetTelephoneNumber()
+        {
+            return TelephoneNumber;
+        }
+
+        public string GetEmail()
+        {
+            return Email;
+        }
+
+        public string GetJobTitle()
+        {
+            return JobTitle;
+        }
+
+        public string GetJobDescription()
+        {
+            return JobDescription;
+        }
+
+        public void SetFIO(string FIO)
+        {
+            this.FIO = FIO;
+        }
+
+        public void SetBirthDate(string BirthDate)
+        {
+            this.BirthDate=BirthDate;
+        }
+
+        public void SetTelephoneNumber(string TelephoneNumber)
+        {
+            this.TelephoneNumber= TelephoneNumber;
+        }
+
+        public void SetEmail(string Email)
+        {
+            this.Email= Email;
+        }
+
+        public void SetJobTitle(string JobTitle)
+        {
+            this.JobTitle= JobTitle;
+        }
+
+        public void SetJobDescription(string JobDescription)
+        {
+            this.JobDescription= JobDescription;
+        }
+
+        
+
+    }
+
+    public class Shape
+    {
+        public double SquareOf(double a)
+        {
+            return 6 * a * a;
+        }
+
+        public double SquareOf(double hight, double width, double length)
+        {
+            return 2*(hight*width + hight*length + width*length);
+        }
+    }
+
+    public class Distance
+    {
+        private double a = 1;
+        private double b = 2;
+
+        public void Print()
+        {
+            Console.WriteLine("a = " + a + ", b = " + b);
+        }
+
+        public double Summ()
+        {
+            return a + b;
+        }
+
+        public double MaxOf()
+        {
+            return a > b ? a : b;
+        }
     }
 }
